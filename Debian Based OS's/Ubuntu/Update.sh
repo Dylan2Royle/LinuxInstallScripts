6f9a3e922e97e43de1765875276885207cd9b735
Dylan Royle <enquiries@dylanroyle.co.uk>
2024-10-21 10:36:45 +0100

    Update Update.sh

--- Debian Based OS's/Ubuntu/Update.sh
+++ Debian Based OS's/Ubuntu/Update.sh
@@ -8,27 +8,3 @@ echo "System updated"
 # Install sudo with automatic confirmation
 apt install sudo -y
 echo "Sudo is installed"
-
-# Ask the user to make a username and password
-read -p "Enter username: " username
-
-# Prompt for password with hidden input
-read -s -p "Enter password: " password
-echo
-read -s -p "Confirm password: " password_confirm
-echo
-
-# Check if password and confirmation match
-if [ "$password" != "$password_confirm" ]; then
-  echo "Passwords do not match."
-  exit 1
-fi
-
-# Create user and set password
-sudo useradd -m "$username"
-echo "$username:$password" | sudo chpasswd
-
-# Add the new user to the sudo group
-sudo usermod -aG sudo "$username"
-
-echo "User $username has been created and added to sudo group."
