cc55e41c478b5d9fa275c9649fa31a890562bca0
Dylan Royle <enquiries@dylanroyle.co.uk>
2024-10-21 10:31:31 +0100

    Update Update.sh

--- Debian Based OS's/Ubuntu/Update.sh
+++ Debian Based OS's/Ubuntu/Update.sh
@@ -1,3 +1,20 @@
+#!/bin/bash
+
 apt-get update -y
 apt-get upgrade -y
-echo "
+echo "Updated"
+apt install sudo
+echo "Sudo is installed"
+# Ask the user to make a username and password 
+
+
+read -p "Enter username: " username
+
+read -s -p "Enter password: " password
+echo
+
+sudo useradd -m "$username"
+
+echo "$username:$password" | sudo chpasswd
+
+echo "User $username has been created."
